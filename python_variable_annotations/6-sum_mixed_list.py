9c32c77e9fe11de3d72f2b9d95f09d8999ab379f
TuyenH <thtuyen199@gmail.com>
2024-04-23 08:34:53 +0000

    modify shebang line

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """A complex types-annotated function mixed_list"""
 from typing import List, Union
 
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Duck type an iterable object"""
 from typing import Iterable, Sequence, List, Tuple
 
