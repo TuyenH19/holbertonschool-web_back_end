6fc1fa30936c630d939c4ba9cc8431a64cc49d54
TuyenH <thtuyen199@gmail.com>
2024-04-23 08:39:03 +0000

    modify return typing.Tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[Union[str, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuble of string and float."""
     return [k, v ** 2]
