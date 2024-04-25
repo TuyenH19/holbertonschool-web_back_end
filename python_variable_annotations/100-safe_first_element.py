a9cf6e0b482873fc47c224f5d4004a5b3fa0541b
TuyenH <thtuyen199@gmail.com>
2024-04-25 08:50:33 +0000

    Change typing from union to optional

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -8,10 +8,10 @@ Augment the following code with the correct duck-typed annotations:
         else:
             return None
 """
-from typing import Sequence, Any, Union
+from typing import Sequence, Any, Optional
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """ Duck typing - first element of a sequence """
     if lst:
         return lst[0]
