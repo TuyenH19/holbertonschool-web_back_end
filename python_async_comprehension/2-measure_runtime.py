69c82d7a89cb72ebebc0992b52ce5a7ea38d0dba
TuyenH <thtuyen199@gmail.com>
2024-04-26 09:57:03 +0000

    modify gather function

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,6 +2,7 @@
 """Run time for four parallel comprehensions."""
 import asyncio
 import time
+
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
@@ -9,15 +10,6 @@ async def measure_runtime() -> float:
     """Asynchronous coroutine that will execute async_comprehension
     4 times in parallel using asyncio.gather, the return the running time."""
     start_time = time.time()
-
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.time()
-    total_time = end_time - start_time
-
-    return total_time
+    return end_time - start_time
