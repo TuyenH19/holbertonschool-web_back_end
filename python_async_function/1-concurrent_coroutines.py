e66ac42990551e497f5461cf7f243934d373e28b
TuyenH <thtuyen199@gmail.com>
2024-04-26 10:08:43 +0000

    modify wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """Execute multiple coroutines at the same time with async."""
+
 import asyncio
-from typing import List
+import typing
 
 # import wait_random
-wait_random = __import__('0-basic_async_syntax').wait_random 
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """Asynchronous coroutine that takes in 2 int arguments
     n and max_delay and return list of all delays
     in order without using sort()."""
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    done, _ = await asyncio.wait(tasks)
-    return sorted([task.result() for task in done])
+    delay_n = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+
+    return sorted(delay_n)
