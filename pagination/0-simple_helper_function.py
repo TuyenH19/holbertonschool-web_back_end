2fb7ea2c441de388329e82f7c7f706e69415491a
TuyenH <thtuyen199@gmail.com>
2024-05-02 10:44:09 +0200

    modify code for pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """Simple helper function"""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return a tuple of size two containing a start index and an end index"""
     start_index = (page - 1) * page_size
