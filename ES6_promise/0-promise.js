0fcddd17cad00331b83cc178649929a4199c29aa
TuyenH <thtuyen199@gmail.com>
2024-05-16 08:11:11 +0000

    Task0- check if response variable is an instance of a Promise.

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+// 0-promise.js
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Here, you would typically make an API request
+    // For simplicity, let's resolve the Promise immediately
+    resolve("Response from API");
+  });
+}
+
+export default getResponseFromAPI;
