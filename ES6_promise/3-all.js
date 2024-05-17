2964f4c4d66fc9b92d0ed6eb715460eb19dfd589
TuyenH <thtuyen199@gmail.com>
2024-05-17 06:11:30 +0000

    modify array

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
