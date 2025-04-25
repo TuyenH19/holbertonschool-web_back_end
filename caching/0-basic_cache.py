43f6737625953dc794c76a363128495b4c49ecc6
TuyenH <thtuyen199@gmail.com>
2025-04-25 08:03:45 +0000

    create basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,22 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
   Basic dictionary
 """
 import sys
-from ./basic_caching import BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-  """ Basic caching system with no limit """
+    """Basic caching system with no limit"""
 
-  def put(self, key, item):
-    """ Add an item to the cache """
-    if key is not None or item is not None:
-      self.cache_data[key] = item
-      return item
+    def put(self, key, item):
+        """Add an item to the cache"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+            return item
 
-  def get(self, key):
-    """Retrieve item from cache """
-    if key is None or key not in self.cache_data:
-      return None
-    return self.cache_data[key]
+
+    def get(self, key):
+        """Retrieve item from cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
