f7ccec9a30e61b0c5d558168a0546c2da07ab2d4
TuyenH <thtuyen199@gmail.com>
2025-04-25 13:55:11 +0000

    modify the code in put as requirement

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -11,12 +11,12 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item to the cache"""
-        if key is not None or item is not None:
-            self.cache_data[key] = item
-            return item
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         """Retrieve item from cache"""
         if key is None or key not in self.cache_data:
-            return None
+            return
         return self.cache_data[key]
