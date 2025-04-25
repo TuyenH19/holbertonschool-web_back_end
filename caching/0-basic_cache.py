af618a1381ea14ae5d68821169f06c264b3ac4e8
TuyenH <thtuyen199@gmail.com>
2025-04-25 08:00:16 +0000

    basic dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+  Basic dictionary
+"""
+import sys
+from ./basic_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+  """ Basic caching system with no limit """
+
+  def put(self, key, item):
+    """ Add an item to the cache """
+    if key is not None or item is not None:
+      self.cache_data[key] = item
+      return item
+
+  def get(self, key):
+    """Retrieve item from cache """
+    if key is None or key not in self.cache_data:
+      return None
+    return self.cache_data[key]
