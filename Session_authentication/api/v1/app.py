661115da2cd8f2b17d22ed579c727b2e04a4ffbb
TuyenH <thtuyen199@gmail.com>
2025-06-30 05:15:43 +0000

    create before request method

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -42,11 +42,13 @@ def before_request():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
 
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and \
+                auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
@@ -72,6 +74,12 @@ def forbidden(error) -> str:
     """Handle a forbidden resource
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+request.current_user = auth.current_user(request):
+    if auth.session_cookie(request) is None:
+        abort(401)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
