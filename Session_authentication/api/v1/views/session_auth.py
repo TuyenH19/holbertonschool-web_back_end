88f9d1dca6c051fb5fd144aa94ffd5f6822757ac
TuyenH <thtuyen199@gmail.com>
2025-06-30 20:06:53 +0000

    fix route for user logout with auth_session

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -39,10 +39,8 @@ def login():
     return response
 
 
-app_views.route('/auth_session/logout', methods=['DELETE'],
-                strict_slashes=False)
-
-
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def logout():
     """ Handle user logout with session authentication """
     from api.v1.app import auth
