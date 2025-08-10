be5202418440b1b5a2ddd72b515b62c3c0a98816
TuyenH <thtuyen199@gmail.com>
2025-08-10 19:08:10 +0000

    set method private

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -15,6 +15,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """Return a new UUID as a string (module-private helper)."""
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database."""
 
@@ -55,7 +60,3 @@ class Auth:
         except ValueError:
             # if stored_hash is malformed for any reason
             return False
-
-    def _generate_uuid() -> str:
-        """Return a new UUID as a string (module-private helper)."""
-        return str(uuid4())
