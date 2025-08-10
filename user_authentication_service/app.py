ca8b869c191aff830ad1f81ed3d7d2dcc434f32e
TuyenH <thtuyen199@gmail.com>
2025-08-10 19:24:05 +0000

    implement a login function

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -23,6 +23,20 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    resp = jsonify({"email": email, "message": "logged in"})
+    resp.set_cookie("session_id", session_id)
+    return resp
 
 
 if __name__ == "__main__":
