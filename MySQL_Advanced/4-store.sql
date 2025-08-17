b6dad0b5758bde28c34b2b1bea07599b167e030e
TuyenH <thtuyen199@gmail.com>
2025-08-17 21:29:51 +0000

    delete setting begin - end

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -2,8 +2,6 @@
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
-END;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
