37e991c545150e04773d36dbdd57e9c1e6914f3e
TuyenH <thtuyen199@gmail.com>
2025-08-17 20:13:12 +0000

    modify comment lines for sql file

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
---- SQL script to create a table users with following attributes:
---- id (INT, PRIMARY KEY, NOT NULL, AUTO INCREMENT)
---- email (VARCHAR(255), NOT NULL, UNIQUE)
---- name (VARCHAR(255))
+-- SQL script to create a table users with following attributes:
+-- id (INT, PRIMARY KEY, NOT NULL, AUTO INCREMENT)
+-- email (VARCHAR(255), NOT NULL, UNIQUE)
+-- name (VARCHAR(255))
 CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
