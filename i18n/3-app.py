a8a2d0691f7ed036000b294ff7a42fb7f108f428
TuyenH <thtuyen199@gmail.com>
2025-09-06 08:33:22 +0000

    fix typo error

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -21,7 +21,7 @@ babel = Babel(app)
 
 def get_locale():
     """Pick best match from Accept-Language header."""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 # Initialize Babel with custom locale selector
