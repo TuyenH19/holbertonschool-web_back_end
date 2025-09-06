c2eaa97564dca8ce3970e01406d7599a0935ce61
TuyenH <thtuyen199@gmail.com>
2025-09-06 08:28:17 +0000

    add comments

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
+"""
+Basic Flask app with Babel and internationalization
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+app = Flask(__name__)
+
 
 class Config:
-    """Configuration for Babel."""
+    """Configuration for Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    # Point Babel to the translations directory
-    BABEL_TRANSLATION_DIRECTORIES = "translations"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-
-# Initialize Babel
-babel = Babel()
+babel = Babel(app)
 
 
 def get_locale():
     """Pick best match from Accept-Language header."""
-    lang = request.args.get("lang")
-    if lang in app.config["LANGUAGES"]:
-        return lang
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
@@ -33,8 +30,5 @@ babel.init_app(app, locale_selector=get_locale)
 
 @app.route('/')
 def index():
+    """Render the index page."""
     return render_template('3-index.html')
-
-
-if __name__ == "__main__":
-    app.run(port=5000, host="localhost")
