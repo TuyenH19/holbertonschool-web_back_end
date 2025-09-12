3f5cd9e44a27ba77501db5be47482caac271d164
TuyenH <thtuyen199@gmail.com>
2025-09-12 11:42:29 +0000

    create file utils.js

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,21 @@
+// utils.js
+const Utils = {
+  calculateNumber(type, a, b) {
+    const A = Math.round(a);
+    const B = Math.round(b);
+
+    switch (type) {
+      case 'SUM':
+        return A + B;
+      case 'SUBTRACT':
+        return A - B;
+      case 'DIVIDE':
+        if (B === 0) return 'Error';
+        return A / B;
+      default:
+        throw new TypeError('Invalid type. Use SUM, SUBTRACT, or DIVIDE.');
+    }
+  }
+};
+
+module.exports = Utils;
