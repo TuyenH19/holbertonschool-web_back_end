b3800960c89274016d9236ac1b2164bdd148515f
TuyenH <thtuyen199@gmail.com>
2025-11-14 00:03:27 +0000

    add document

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+"""
+Test utils module.
+"""
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
@@ -17,6 +19,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map function."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -24,6 +27,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), "b"),
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        """Test access_nested_map function with exception."""
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), f"'{expected_key}'")
@@ -38,6 +42,7 @@ class TestGetJson(unittest.TestCase):
         ("http://holberton.io", {"payload": False}),
     ])
     def test_get_json(self, test_url, test_payload):
+        """Test get_json function."""
         with patch("utils.requests.get") as mock_get:
             mock_resp = Mock()
             mock_resp.json.return_value = test_payload
@@ -54,7 +59,7 @@ class TestMemoize(unittest.TestCase):
     TestMemoize class to test memoize decorator.
     """
     def test_memoize(self):
-        """Caches value so the underlying method is called once."""
+        """Test memoize decorator."""
         class TestClass:
             def a_method(self):
                 return 42
